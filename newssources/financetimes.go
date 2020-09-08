3f1543f3996f31b63c125ad47e72b0942130b93d
Nav-aggarwal09 <arnav0908@gmail.com>
2020-09-07 18:43:03 -0700

    added login.html

--- newssources/financetimes.go
+++ newssources/financetimes.go
@@ -9,7 +9,7 @@ import (
 
 func Test() {
 	getcall := fmt.Sprintf("https://api.ft.com/content/7348edd8-0403-11de-845b-000077b07658/v1?apiKey=%v",
-		"")
+		" ")
 	initialftresponse, err :=
 		http.Get(getcall)
 
